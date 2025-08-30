c534de15d27cc27591f4f266f5cd358dda1e133c
milan vadhiya <vadhiyamilan907@gmail.com>
2025-08-30 18:23:16 +0530

    exploring routed http method with postmen

--- Homework.md
+++ Homework.md
@@ -6,3 +6,15 @@
 - Listen on port 3000,
 - Write a request handles for '/test','/hello',
 - Install nodemon and update Scripts Inside package.json.
+
+- initilize git,
+- .gitignore,
+- create a remote repo on github,
+- push all code to remote origin,
+- play with rout extension and route extension ex: /hello , / , hello/2 , /xyz....
+- Order of a route metter a lot,
+
+- install postman app and make a workspace/collection > test api called....
+- write a logic of handle get, post, patch, delete, put api called and test them on a postman
+
+
--- src/app.js
+++ src/app.js
@@ -3,13 +3,43 @@ const express = require("express");
 const app = express();
 const port=3000;
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello ! Hello! hello !! :) :) :)");
+
+
+
+
+// app.use("/hello/2",(req,res)=>{
+//     res.send("Abraka dabra.. :) :) :)");
+// });
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello ! Hello! hello !! :) :) :)");
+// });
+
+// app.use("/test",(req,res)=>{
+//     res.send("Hello from express server :) :) :)");
+// });
+
+app.use("/user",(req,res)=>{
+    res.send("handled by use!!!");
 });
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from express server :) :) :)");
+app.get("/user",(req,res)=>{
+    res.send({firstName:"John",lastName:"Doe"});
+});
+
+app.post("/user",(req,res)=>{
+    console.log("Data has been saved to Database....!");
+    res.send("Data has been saved successfully to Database");
+});
+app.delete("/user",(req,res)=>{
+    res.send("User has been deleted successfully from Database !!!");
 });
 app.listen(port,()=>{
     console.log(`server is running at port ${port}.....`);
 });
+
+// app.use("/",(req,res)=>{
+//     res.send("Hello  :) :) :)");
+// });
+
+
+
